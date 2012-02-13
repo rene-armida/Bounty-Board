08a5f8b51a0548e9b8a260c7ef11394561845de9
Orion Auld <orion.auld@gmail.com>
2012-02-12 21:31:57 -0800

    initial checkin

---
+++ bounty/bounty/models/__init__.py
@@ -0,0 +1,4 @@
+import settings.active as settings
+from questions.quiz.models.quiz import Quiz 
+from questions.quiz.models.question import Question 
+
---
+++ bounty/settings/active.py
@@ -0,0 +1,1 @@
+from questions.settings.dev import *
