261d4a478e8259f23facc77936c47e30d7e5eef6
Michael Armida <me@marmida.org>
2012-02-20 17:10:39 -0800

    fetch access token from meetup, print it

---
+++ bounty/board/views/oauth2.py
@@ -0,0 +1,67 @@
+from django.conf import settings
+import django.http
+from django.shortcuts import render_to_response
+
+import json
+import urllib
+import urllib2
+
+
+def require_https(fn):
+	'''
+	decorate a view to raise 403 (forbidden) if we're not connected via HTTPS
+
+	replace this with 'raise Http403' when that gets released
+	see: https://docs.djangoproject.com/en/dev/topics/http/views/#the-403-http-forbidden-view
+	'''
+	def protected_view(request):
+		if not request.is_secure():
+			return django.http.HttpResponse(status=403, content='Forbidden')
+		# otherwise, invoke the view as usual
+		return fn(request)
+	return protected_view
+
+def login(request):
+	dest = settings.MEETUP_AUTHORIZATION_URL + \
+		('?client_id=%s&response_type=code&redirect_uri=%s' % (settings.MEETUP_OAUTH2_CLIENT_ID, settings.MEETUP_REDIRECT_URI))
+
+	return render_to_response(
+		'login.html', 
+		{
+			'dest': dest,
+		}
+	)
+
+@require_https
+def authorize(request):
+	'''
+	receive an auth code from Meetup; store the auth code and state
+	'''
+	if 'error' in request.GET:
+		# this authorization request was not successful
+		if request.GET['error'] == 'access_denied':
+			# the meetup user denied us; explain this on our end
+			return render_to_response('access_denied.html')
+		
+		# otherwise, there must be a programming or setup error; 
+		# raise an Exception describing the problem, so it gets logged
+		raise Exception('Meetup authorization failed; error: %s' % request.GET['error'])
+
+	# we were successfully authorized, post to the Meetup token endpoint
+	post_data = urllib.urlencode({
+		'client_id': settings.MEETUP_OAUTH2_CLIENT_ID,
+		'grant_type': 'authorization_code',
+		'redirect_uri': settings.MEETUP_REDIRECT_URI,
+		'code': request.GET['code']
+	})
+	# note: urllib2.urlopen doesn't verify server's cert; is this a security hole?
+	response = urllib2.urlopen(settings.MEETUP_TOKEN_URL, post_data)
+
+	# interpret the response as JSON content; both success and failure use this response type
+	# if a lower-level failure occurred, like the connection not being made, we assume urlopen
+	# will have raised a URLError.
+	response = json.loads(response)
+	if 'error' in response:
+		raise Exception('Meetup access token request failed: %s' % response['error'])
+	
+	return django.http.HttpResponse(content='access token: %s; refresh token: %s' % (response['access_token'], response['refresh_token']))
--- bounty/settings.py
+++ bounty/settings.py
@@ -1,4 +1,6 @@
-# Django settings for bounty project.
+'Django settings for bounty project.'
+
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',    # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db/questions.db',                 # Or path to database file if using sqlite3.
+        'NAME': '/home/marmida/develop/bounty-board/db/bounty.db',                 # Or path to database file if using sqlite3.
         'USER': '',                                # Not used with sqlite3.
         'PASSWORD': '',                            # Not used with sqlite3.
         'HOST': '',                                # Set to empty string for localhost. Not used with sqlite3.
@@ -115,11 +117,13 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'djangorestframework',
     'board',
-    'south',   # should be last
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'south',   # should be last
+    
 )
 
 # A sample logging configuration. The only tangible logging
@@ -144,6 +148,11 @@ LOGGING = {
         },
     }
 }
+
+# meetup stuff - if these variables exist in the environment, copy them into this module
+for i in ['MEETUP_AUTHORIZATION_URL', 'MEETUP_OAUTH2_CLIENT_ID', 'MEETUP_TOKEN_URL', 'MEETUP_REDIRECT_URI']:
+    if i in os.environ:
+        globals[i] = os.environ[i]
 
 
 SITE_ID = 1
