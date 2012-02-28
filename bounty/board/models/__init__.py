ea66fd30e460ba7f8d86ffd23c0cd263eeeb5288
Michael Armida <me@marmida.org>
2012-02-27 18:41:00 -0800

    remove debugging, add help text

--- bounty/board/models/__init__.py
+++ bounty/board/models/__init__.py
@@ -7,7 +7,7 @@ class Hack(models.Model):
 	'Represents a project or idea for collaborative effort at Hack Night events'
 	name = models.CharField(max_length=50, help_text='Short name for this idea or project')
 	abstract = models.CharField(max_length=150, help_text='A one-line description', blank=True)
-	description = models.TextField(blank=True)
+	description = models.TextField(blank=True, help_text='Detailed description of your hack, project, or idea.  Use Markdown-style formatting.')
 	source_url = models.URLField(verify_exists=False, help_text='Where to get the source code for this project', 
 		blank=True)
 	author = models.ForeignKey(django.contrib.auth.models.User, help_text='Proposer of this project')
